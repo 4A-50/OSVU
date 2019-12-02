cf7269666d021e71bf7f2ccc7361f66091ed4a5b
Jake Price <scienceguy68@gmail.com>
2019-12-02 15:21:14 +0000

    Set Up Hand Models

---
+++ OSVU/Assets/Scripts/VR/Hand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Interactable")
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
