59848414334fca0e635057edfb8a230d45d3fc5f
Jake Price <scienceguy68@gmail.com>
2019-12-27 18:38:39 +0000

    Added Lights To The Platforms

---
+++ OSVU/Assets/Scripts/LightBlinker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class LightBlinker : MonoBehaviour
+{
+    Light lightToBlink;
+
+    public float blinkSpeed = 0.5f;
+    public bool blinking = true;
+
+    void Start()
+    {
+        lightToBlink = GetComponent<Light>();
+        StartCoroutine(blinker());
+    }
+
+    IEnumerator blinker()
+    {
+        while(blinking == true)
+        {
+            lightToBlink.enabled = true;
+            yield return new WaitForSeconds(blinkSpeed);
+            lightToBlink.enabled = false;
+            yield return new WaitForSeconds(blinkSpeed);
+        }
+       
+    }
+}
