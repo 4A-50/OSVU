4632e2c528de2406ca84b0823fe5525c30930700
Jake Price <scienceguy68@gmail.com>
2020-01-21 11:26:43 +0000

    Created Rack Controller Scripts

---
+++ OSVU/Assets/Editor/RackEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LandingRack))]
+public class RackEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        LandingRack rack = (LandingRack)target;
+        
+        if (GUILayout.Button("Open"))
+        {
+            rack.openRack();
+        }
+
+        if (GUILayout.Button("Close"))
+        {
+            rack.closeRack();
+        }
+    }
+}
---
+++ OSVU/Assets/Scripts/LandingRack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class LandingRack : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void openRack()
+    {
+        anim.SetTrigger("Open");
+        anim.ResetTrigger("Close");
+    }
+
+    public void closeRack()
+    {
+        anim.ResetTrigger("Open");
+        anim.SetTrigger("Close");
+    }
+}
