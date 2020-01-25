039bef753f18532bc91d4ff10e0efa6d990b5e1d
Jake Price <scienceguy68@gmail.com>
2020-01-25 13:15:06 +0000

    Small Interactable changes

--- OSVU/Assets/Scripts/Interaction/Interactable.cs
+++ OSVU/Assets/Scripts/Interaction/Interactable.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [HideInInspector]
+    public Hands activeHand;
+
+    Rigidbody rb = null;
+
+    public Transform primaryAttachmentPoint;
+    public Transform[] secondaryAttachmentsPoints;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public bool InHand()
+    {
+        if (activeHand != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
