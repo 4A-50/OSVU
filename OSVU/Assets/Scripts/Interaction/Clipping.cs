3011e49c3ad11d531897957d90d516a88660f491
Jake Price <scienceguy68@gmail.com>
2020-01-27 11:00:46 +0000

    IK Work

--- OSVU/Assets/Scripts/Interaction/Clipping.cs
+++ OSVU/Assets/Scripts/Interaction/Clipping.cs
@@ -26,29 +26,47 @@ public class Clipping : MonoBehaviour
     [Space]
 
     public LayerMask layer;
+    public Transform followPoint;
 
     Vector3 lastPos;
+
+    void Awake()
+    {
+        lastPos = transform.position;
+    }
 
     void Update()
     {
         switch (overlapType)
         {
             case Type.Box:
-                if (Physics.OverlapBox(transform.position, boxExents, transform.rotation, layer).Length != 0)
+                if (Physics.OverlapBox(followPoint.position, boxExents, transform.rotation, layer).Length != 0)
                 {
                     transform.position = lastPos;
+                }
+                else
+                {
+                    transform.position = followPoint.position;
                 }
                 break;
             case Type.Sphere:
-                if (Physics.OverlapSphere(transform.position, sphereRadius, layer).Length != 0)
+                if (Physics.OverlapSphere(followPoint.position, sphereRadius, layer).Length != 0)
                 {
                     transform.position = lastPos;
                 }
+                else
+                {
+                    transform.position = followPoint.position;
+                }
                 break;
             case Type.Capsule:
-                if (Physics.OverlapCapsule(transform.position + capsule1, transform.position + capsule2, capsuleRadius, layer).Length != 0)
+                if (Physics.OverlapCapsule(followPoint.position + capsule1, transform.position + capsule2, capsuleRadius, layer).Length != 0)
                 {
                     transform.position = lastPos;
+                }
+                else
+                {
+                    transform.position = followPoint.position;
                 }
                 break;
         } 
@@ -63,16 +81,18 @@ public class Clipping : MonoBehaviour
     {
         Gizmos.color = Color.red;
 
+        Gizmos.DrawLine(transform.position, followPoint.position);
+
         switch (overlapType)
         {
             case Type.Box:
-                Gizmos.DrawWireCube(transform.position, boxExents * 2);
+                Gizmos.DrawWireCube(followPoint.position, boxExents * 2);
                 break;
             case Type.Sphere:
-                Gizmos.DrawWireSphere(transform.position, sphereRadius);
+                Gizmos.DrawWireSphere(followPoint.position, sphereRadius);
                 break;
             case Type.Capsule:
-                DebugExtension.DrawCapsule(transform.position + capsule1, transform.position + capsule2, Color.red, capsuleRadius);
+                DebugExtension.DrawCapsule(followPoint.position + capsule1, followPoint.position + capsule2, Color.red, capsuleRadius);
                 break;
         }
     }
